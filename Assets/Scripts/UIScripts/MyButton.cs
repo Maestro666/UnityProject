adaf812829102f8c827566ea58f9f7628672e912
Andrew Maestro <andrewmaestro1991@gmail.com>
2017-06-19 15:07:22 +0300

    Gui

--- Assets/Scripts/UIScripts/MyButton.cs
+++ Assets/Scripts/UIScripts/MyButton.cs
@@ -1,13 +1,23 @@
 ﻿using System.Collections;
 using UnityEngine.Events;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MyButton : MonoBehaviour
 {
-
+    public MyButton playButton;
     public UnityEvent signalOnClick = new UnityEvent();
     public void _onClick()
     {
         this.signalOnClick.Invoke();
+    }
+
+    void Start()
+    {
+        playButton.signalOnClick.AddListener(this.onPlay);
+    }
+    void onPlay()
+    {
+        SceneManager.LoadScene("ChooseLevel");
     }
 }
